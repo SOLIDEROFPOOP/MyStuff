66d995a94dd29bb3fa141e3da42dfe357bf5f237
SOLIDEROFPOOP <paprim2003@gmail.com>
2023-08-28 23:58:33 +0600

    new things idk tutorials and learning projects

--- CarRentApp/src/main/java/com/muratkapparov/carrentapp/CarRentApp/Service/RentService.java
+++ CarRentApp/src/main/java/com/muratkapparov/carrentapp/CarRentApp/Service/RentService.java
@@ -1,8 +1,11 @@
 package com.muratkapparov.carrentapp.CarRentApp.Service;
 
+import com.muratkapparov.carrentapp.CarRentApp.Entity.Rent;
 import com.muratkapparov.carrentapp.CarRentApp.Repository.RentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RentService {
@@ -10,5 +13,8 @@ public class RentService {
     @Autowired
     public RentService(RentRepository rentRepository) {
         this.rentRepository = rentRepository;
+    }
+    public List<Rent> returnAll(){
+        return rentRepository.findAll();
     }
 }
