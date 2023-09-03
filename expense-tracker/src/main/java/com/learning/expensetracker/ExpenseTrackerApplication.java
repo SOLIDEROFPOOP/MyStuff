d3e506e0dc519f16744af32fb1ed2500316a8fcd
SOLIDEROFPOOP <paprim2003@gmail.com>
2023-09-03 19:43:33 +0600

    project update

--- expense-tracker/src/main/java/com/learning/expensetracker/ExpenseTrackerApplication.java
+++ expense-tracker/src/main/java/com/learning/expensetracker/ExpenseTrackerApplication.java
@@ -1,7 +1,11 @@
 package com.learning.expensetracker;
 
+import com.learning.expensetracker.filters.AuthFilter;
+import jakarta.servlet.FilterRegistration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExpenseTrackerApplication {
@@ -9,5 +13,12 @@ public class ExpenseTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
-
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterFilterRegistrationBean(){
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
+	}
 }
--- expense-tracker/src/main/java/com/learning/expensetracker/resources/UserResource.java
+++ expense-tracker/src/main/java/com/learning/expensetracker/resources/UserResource.java
@@ -1,12 +1,16 @@
 package com.learning.expensetracker.resources;
 
+import com.learning.expensetracker.Constants;
 import com.learning.expensetracker.domain.User;
 import com.learning.expensetracker.services.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,17 +26,30 @@ public class UserResource {
         String email = (String) userMap.get("email");
         String password = (String) userMap.get("password");
         User user = userService.registerUser(firstName , lastName , email , password);
-        Map<String , String> map = new HashMap<>();
-        userMap.put("message" , "registered successfully");
-        return new ResponseEntity<>(map , HttpStatus.OK);
+
+        return new ResponseEntity<>(generateJWTToken(user) , HttpStatus.OK);
     }
     @PostMapping("/login")
     public ResponseEntity<Map<String , String>> loginUser(@RequestBody Map<String , Object> userMap){
         String email = (String) userMap.get("email");
         String password = (String) userMap.get("password");
         User user = userService.validateUser(email , password);
+
+        return new ResponseEntity<>(generateJWTToken(user) , HttpStatus.OK);
+    }
+    private Map<String , String> generateJWTToken(User user){
+        long timestamp = System.currentTimeMillis();
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+                .setIssuedAt(new Date(timestamp))
+                .setExpiration(new Date(timestamp + Constants.TOKEN_VALIDITY))
+                .claim("userId" , user.getUserId())
+                .claim("userEmail" , user.getEmail())
+                .claim("userPassword" , user.getPassword())
+                .claim("userFirstName" , user.getFirstname())
+                .claim("userLastName" , user.getLastname())
+                .compact();
         Map<String , String> map = new HashMap<>();
-        map.put("message", "logged successfully");
-        return new ResponseEntity<>(map , HttpStatus.OK);
+        map.put("token" , token);
+        return map;
     }
 }
